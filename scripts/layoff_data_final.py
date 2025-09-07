bdf1ff1a9a3d55f43b1a43f028d050acc9b9c9b5
natelovesmathandcs <nathanko783@gmail.com>
2025-09-07 16:12:52 -0400

    Update layoff_data_final.py

--- scripts/layoff_data_final.py
+++ scripts/layoff_data_final.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+#removes companies without a ticker
 layoff_df = pd.read_csv("/Users/nathanko/PycharmProjects/tech-layoffs-stock-analysis/data/raw/top_100_layoffs_csv", keep_default_na=False, usecols=["Company", "# Laid Off", "%", "Date Added", "Ticker"])
 layoff_df = layoff_df[layoff_df["Ticker"] != "N/A"]
 layoff_df.to_csv("/Users/nathanko/PycharmProjects/tech-layoffs-stock-analysis/data/processed/layoff_data_csv")
